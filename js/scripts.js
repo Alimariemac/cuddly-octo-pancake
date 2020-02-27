7925df142794bb0cdbc71654cce7f1fcf5599500
Alicia MacCara <Aliciamaccara@gmail.com>
2020-02-27 17:22:33 -0500

    styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
 $(function(){
-
+      
     $("#expand-message-01").click(function(){        
         if($("#message-container-01").css("display")=='none' && $(window).width() < 576){
             $("#message-container-01").css('display','block');
         }
-        else if($("#message-container-01").css("display")=='block' ){
+        else if($("#message-container-01").css("display")=='block' && $(window).width() < 576){
             $("#message-container-01").css('display','none');
         }
     });
-    if( $(window).width()>576){
-        $("#message-container-01").css('display','block');
-    } 
 });
